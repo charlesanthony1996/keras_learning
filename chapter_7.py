53a0e756f04dcceb0872624e13345ee1e091c9af
charlesanthony1996 <charlesanthony296@gmail.com>
2025-02-27 09:01:40 +0100

    using callbacks as an arg in the fit method

--- chapter_7.py
+++ chapter_7.py
@@ -252,4 +252,56 @@ predictions = model.predict(test_images)
 import tensorflow as tf
 
 class RootMeanSquaredError(keras.metrics.Metric):
-    pass
+    def __init__(self, name='rmse', **kwargs):
+        super().__init__(name = name, **kwargs)
+        self.mse_sum = self.add_weight(name="mse_sum", initializer="zeros")
+        self.total_samples = self.add_weight(name="total_samples", initializer="zeros", dtype="int32")
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_true = tf.one_hot(y_true, depth=tf.shape(y_pred)[1])
+        mse = tf.reduce_sum(tf.square(y_true - y_pred))
+        self.mse_sum.assign_add(mse)
+        num_samples = tf.shape(y_pred)[0]
+        self.total_samples.assign_add(num_samples)
+
+    def result(self):
+        return tf.sqrt(self.mse_sum/ tf.cast(self.total_samples, tf.float32))
+
+    def reset_state(self):
+        self.mse_sum.assign(0.)
+        self.total_samples.assign(0)
+
+model = get_mnist_model()
+model.compile(
+    optimizer="rmsprop",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy", RootMeanSquaredError()]
+)
+
+model.fit(train_images, train_labels, epochs=3, validation_data=(val_images, val_labels))
+test_metrics = model.evaluate(test_images, test_labels)
+
+# listing 7.19 using the callbacks argument in the fit() method
+
+callbacks_list = [
+    keras.callbacks.EarlyStopping(
+        monitor="val_accuracy",
+        patience=2,
+    ),
+    keras.callbacks.ModelCheckpoint(
+        filepath="checkpoint_path.keras",
+        monitor="val_loss",
+        save_best_only=True,
+    )
+]
+
+model = get_mnist_model()
+model.compile(
+    optimizer="rmsprop",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.fit(train_images, train_labels, epochs=10, callbacks=callbacks_list, validation_data=(val_images, val_labels))
+
+model = keras.models.load_model("checkpoint_path.keras")
