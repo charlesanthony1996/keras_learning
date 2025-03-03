c8a3c27909111f7c785426d326c3ac280daa0a47
charlesanthony1996 <charlesanthony296@gmail.com>
2025-03-03 22:18:03 +0100

    fixed some typos

--- chapter_7.py
+++ chapter_7.py
@@ -369,16 +369,16 @@ for value in values:
 
 model = get_mnist_model()
 
-loss_fn = keras.losses.SparseCategoricalAccuracy()
-optimizer = keras.optimizer.RMSProp()
+loss_fn = keras.losses.SparseCategoricalCrossentropy()
+optimizer = keras.optimizers.RMSprop()
 metrics = [keras.metrics.SparseCategoricalAccuracy()]
 loss_tracking_metric = keras.metrics.Mean()
 
 def train_step(inputs, targets):
     with tf.GradientTape() as tape:
-        predictions = model(inputs, predictions)
+        predictions = model(inputs, training=True)
         loss = loss_fn(targets, predictions)
-    gradients = tape.gradient(loss, model.training_weights)
+    gradients = tape.gradient(loss, model.trainable_weights)
     optimizer.apply_gradients(zip(gradients, model.trainable_weights))
 
     logs = {}
@@ -394,4 +394,47 @@ def train_step(inputs, targets):
 # listing 7.22 writing a step by step training loop: resetting the metrics
 
 def reset_metrics():
-    pass
+    for metric in metrics:
+        metric.reset_state()
+    loss_tracking_metric.reset_state()
+
+
+training_dataset = tf.data.Dataset.from_tensor_slices(
+    (train_images, train_labels)
+)
+training_dataset = training_dataset.batch(32)
+epochs = 1
+for epoch in range(epochs):
+    reset_metrics()
+    for inputs_batch, targets_batch in training_dataset:
+        logs = train_step(inputs_batch, targets_batch)
+    print(f"results at the end of epoch {epoch}")
+    for key, value in logs.items():
+        print(f"...{key}: {value:.4f}")
+
+
+# listing 7.24 writing a step by step evaluation loop
+
+def test_step(inputs, targets):
+    predictions = model(inputs, training=False)
+    loss = loss_fn(targets, predictions)
+
+    logs = {}
+    for metric in metrics:
+        metric.update_state(targets, predictions)
+        logs["val_" + metric.name] = metric.result()
+
+    loss_tracking_metric.update(loss)
+    logs["val_loss"] = loss_tracking_metric.result()
+    return logs
+
+
+val_dataset = tf.data.Dataset.from_tensor_slices((val_images, val_labels))
+val_dataset = val_dataset.batch(32)
+reset_metrics()
+
+for inputs_batch , targets_batch in val_dataset:
+    logs = test_step(inputs_batch, targets_batch)
+print("Evaluation result: ")
+for key, value in logs.items():
+    print(f"...{key} : {value:.4f}")
