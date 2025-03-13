7e4d1017085e9fcba67fe47f3990546de8a3c309
charlesanthony1996 <charlesanthony296@gmail.com>
2025-03-13 11:03:52 +0100

    imdb dataset model evaluation

--- chapter_4.py
+++ chapter_4.py
@@ -85,27 +85,27 @@ print(history_dict.keys())
 # listing 4.8 plotting the training and validation loss
 
 import matplotlib.pyplot as plt
-history_dict = history.history
-loss_values = history_dict["loss"]
-val_loss_values = history_dict["val_loss"]
-epochs = range(1, len(loss_values) + 1)
-plt.plot(epochs, loss_values, "bo", label="Training loss")
-plt.plot(epochs, val_loss_values, "b", label="Validation loss")
-plt.title("Training and validation loss")
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
+# history_dict = history.history
+# loss_values = history_dict["loss"]
+# val_loss_values = history_dict["val_loss"]
+# epochs = range(1, len(loss_values) + 1)
+# plt.plot(epochs, loss_values, "bo", label="Training loss")
+# plt.plot(epochs, val_loss_values, "b", label="Validation loss")
+# plt.title("Training and validation loss")
+# plt.xlabel("Epochs")
+# plt.ylabel("Loss")
 # plt.show()
 
 # listing 4.9 plotting the training and validation accuracy
 
-plt.clf()
-acc = history_dict["accuracy"]
-val_acc = history_dict["val_accuracy"]
-plt.plot(epochs, acc, "bo", label="Training accuracy")
-plt.plot(epochs, val_acc, "b", label="Validation accuracy")
-plt.title("Training and validation accuracy")
-plt.xlabel("Epochs")
-plt.ylabel("Accuracy")
+# plt.clf()
+# acc = history_dict["accuracy"]
+# val_acc = history_dict["val_accuracy"]
+# plt.plot(epochs, acc, "bo", label="Training accuracy")
+# plt.plot(epochs, val_acc, "b", label="Validation accuracy")
+# plt.title("Training and validation accuracy")
+# plt.xlabel("Epochs")
+# plt.ylabel("Accuracy")
 # plt.show()
 
 # listing 4.10 retraining a model from scratch
@@ -150,4 +150,113 @@ reverse_word_index = dict(
 decoded_newswire = " ".join([reverse_word_index.get(i - 3, "?") for i in train_data[0]])
 
 print(train_labels[10])
+
+# listing 4.13 encoding the input data
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+# listing 4.14 encoding the labels
+
+def to_one_hot(labels, dimension=46):
+    results = np.zeros((len(labels), dimension))
+
+    for i, label in enumerate(labels):
+        results[i, label] = 1.
+    return results
+
+y_train = to_one_hot(train_labels)
+y_test = to_one_hot(test_labels)
+
+from tensorflow.keras.utils import to_categorical
+y_train = to_categorical(train_labels)
+y_test = to_categorical(test_labels)
+
+
+# listing 4.15 model definition
+
+model = keras.Sequential([
+    layers.Dense(64, activation="relu"),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(46, activation="softmax")
+])
+
+# listing 4.16 compiling the model
+
+model.compile(
+    optimizer="rmsprop",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+
+# listing 4.17 setting aside a validation set
+
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+y_val = y_train[:1000]
+partial_y_train = y_train[1000:]
+
+# listing 4.18 training the model
+
+history = model.fit(
+    partial_x_train,
+    partial_y_train,
+    epochs=20,
+    batch_size=512,
+    validation_data=(x_val, y_val)
+)
+
+# listing 4.19 plotting the training and validation loss
+
+# loss = history.history["loss"]
+# val_loss = history.history["val_loss"]
+# epochs = range(1, len(loss) + 1)
+# plt.plot(epochs, loss, "bo", label="Training loss")
+# plt.plot(epochs, val_loss, "b", label="Validation loss")
+# plt.title("Training and validation loss")
+# plt.legend()
+# plt.show()
+
+
+
+# listing 4.20 plotting the training and validation accuracy
+
+# plt.clf()
+# acc = history.history["accuracy"]
+# val_acc = history.history["val_accuracy"]
+# epochs = range(1, len(acc) + 1)
+# plt.plot(epochs, acc, "bo", label="Training accuracy")
+# plt.plot(epochs, val_acc, "b", label="Validation accuracy")
+# plt.title("Training and validation accuracy")
+# plt.xlabel("Epochs")
+# plt.ylabel("Accuracy")
+# plt.legend()
+# plt.show()
+
+
+# listing 4.21 retraining a model from scratch
+
+model = keras.Sequential([
+    layers.Dense(64, activation="relu"),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(46, activation="softmax")
+])
+
+model.compile(
+    optimizer="rmsprop",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.fit(
+    x_train,
+    y_train,
+    epochs=9,
+    batch_size=512,
+)
+
+results = model.evaluate(x_test, y_test)
+# print(results)
+
 
