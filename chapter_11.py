dc0a47b420c618ead718fb8dad0ee468d607e6c2
charlesanthony1996 <charlesanthony296@gmail.com>
2025-03-21 17:14:31 +0100

    the use of bigrams in text vectorization

--- chapter_11.py
+++ chapter_11.py
@@ -18,20 +18,99 @@
 #         shutil.move(train_dir / category / fname, val_dir / category / fname)
 
 
-# from tensorflow import keras
-# batch_size = 32
+from tensorflow import keras
+from tensorflow.keras.layers import TextVectorization
+batch_size = 32
 
-# train_ds = keras.utils.text_dataset_from_directory(
-#     "aclImdb/train", batch_size=batch_size
-# )
+train_ds = keras.utils.text_dataset_from_directory(
+    "aclImdb/train", batch_size=batch_size
+)
 
-# val_ds = keras.utils.text_dataset_from_directory(
-#     "aclImdb/val", batch_size=batch_size
-# )
+val_ds = keras.utils.text_dataset_from_directory(
+    "aclImdb/val", batch_size=batch_size
+)
 
-# test_ds = keras.utils.text_dataset_from_directory(
-#     "aclImdb/test", 
-# )
+test_ds = keras.utils.text_dataset_from_directory(
+    "aclImdb/test", 
+)
 
 # listing 11.2 displaying the shapes and dtypes of the first batch
+
+for inputs, targets in train_ds:
+    print("inputs shape: ", inputs.shape)
+    print("inputs dtype: ", inputs.dtype)
+    print("targets shape: ", targets.shape)
+    print("targets dtype: ", targets.dtype)
+    print("inputs[0]: ", inputs[0])
+    print("targets[0]: ", targets[0])
+    break
+
+
+
+
+# listing 11.3 preprocessing our datasets with a textvectorization layer
+
+text_vectorization = TextVectorization(max_tokens=20000, output_mode = "multi_hot")
+text_only_train_ds = train_ds.map(lambda x, y: x)
+text_vectorization.adapt(text_only_train_ds)
+
+binary_lgram_train_ds = train_ds.map(
+    lambda x, y: (text_vectorization(x), y), num_parallel_calls=4
+)
+
+binary_lgram_val_ds = val_ds.map(
+    lambda x, y: (text_vectorization(x), y), num_parallel_calls=4
+)
+
+binay_lgram_test_ds = test_ds.map(
+    lambda x, y: (text_vectorization(x), y), num_parallel_calls=4 
+)
+
+# listing 11.4 inspecting the output of our binary unigram dataset
+
+for inputs, targets in binary_lgram_train_ds:
+    print("inputs shape: ", inputs.shape)
+    print("inputs dtype: ", inputs.dtype)
+    print("targets shape: ", targets.shape)
+    print("targets dtype: ", targets.dtype)
+    print("inputs[0]: ", inputs[0])
+    print("targets[0]: ", targets[0])
+    break
+
+
+# listing 11.5 our model building utility
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def get_model(max_tokens=20000, hidden_dim=16):
+    inputs = keras.Input(shape=(max_tokens,))
+    x = layers.Dense(hidden_dim, activation="relu")(inputs)
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+    model = keras.Model(inputs, outputs)
+    model.compile(
+        optimizer="rmsprop", 
+        loss="binary_crossentropy", 
+        metrics=["accuracy"]
+    )
+    return model
+
+# listing 11.6 training and testing the binary unigram model
+
+model = get_model()
+model.summary()
+callbacks = [
+    keras.callbacks.ModelCheckpoint("binary_lgram.keras", save_best_only=True)
+]
+model.fit(
+    binary_lgram_train_ds.cache(), 
+    validation_data=binary_lgram_val_ds.cache(), 
+    epochs=10, 
+    callbacks=callbacks
+)
+model = keras.models.load_model("binary_lgram.keras")
+print(f"Test acc: {model.evaluate(binay_lgram_test_ds)[1]:.3f}")
+
+# listing 11.7 configuring the text vectorization layer to return bigrams
 
