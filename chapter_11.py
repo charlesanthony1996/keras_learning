80d37b72ee2b4289b163d2a6d50292fb78c4479d
charlesanthony1996 <charlesanthony296@gmail.com>
2025-03-25 10:55:16 +0100

    full transformer encoder

--- chapter_11.py
+++ chapter_11.py
@@ -431,3 +431,198 @@ model = keras.models.load_model("glove_embeddings_sequence_model.keras")
 print(f"Test acc: {model.evaluate(int_test_ds)[1]:.3f}")
 
 
+def self_attention(input_sequence):
+    output = np.zeros(shape=(input_sequence.shape))
+    for i, pivot_vector in enumerate(input_sequence):
+        scores = np.zeros(shape=(len(input_sequence),))
+        for j, vector in enumerate(input_sequence):
+            scores[j] = np.dot(pivot_vector, vector.T)
+        scores /= np.sqrt(input_sequence.shape[1])
+        scores = softmax(scores)
+        new_pivot_representation = np.zeros(shape=pivot_vector.shape)
+        for j, vector in enumerate(input_sequence):
+            new_pivot_representation += vector * scores[j]
+        output[j] = new_pivot_representation
+    return output
+
+
+
+# num_heads = 4
+# embed_dim = 256
+# mha_layer = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+# outputs = mha_layer(inputs, inputs, inputs)
+
+# listing 11.21 Transformer encoder implemented as a subclassed layer
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+class TransformerEncoder(layers.Layer):
+    def __init__(self, embed_dim, dense_dim, num_heads, **kwargs):
+        super().__init__(**kwargs)
+        self.embed_dim = embed_dim
+        self.dense_dim = dense_dim
+        self.num_heads = num_heads
+        self.attention = layers.MultiHeadAttention(
+            num_heads= num_heads,
+            key_dim= embed_dim
+        )
+        self.dense_proj = keras.Sequential(
+            [layers.Dense(dense_dim, activation="relu"), layers.Dense(embed_dim)]
+        )
+
+        self.layernorm_1 = layers.LayerNormalization()
+        self.layernorm_2 = layers.LayerNormalization()
+
+def call(self, inputs, mask=None):
+    if mask is not None:
+        mask = mask[:, tf.newaxis, :]
+    attention_output = self.attention(
+        inputs, inputs, attention_mask=mask
+    )
+    proj_input = self.layernorm_1(inputs + attention_output)
+    proj_output = self.dense_proj(proj_output)
+    return self.layernorm_2(proj_input + proj_output)
+
+def get_config(self):
+    config = super().get_config()
+    config.update({
+        "embed_dim": self.embed_dim,
+        "num_heads": self.num_heads,
+        "dense_dim": self.dense_dim
+    })
+    return config
+
+
+def layer_normalization(batch_of_sequences):
+    mean = np.mean(batch_of_sequences, keepdims=True, axis=-1)
+    variance = np.var(batch_of_sequences, keepdims=True, axis=-1)
+    return (batch_of_sequences - mean) / variance
+
+def batch_normalization(batch_of_images):
+    mean = np.mean(batch_of_images, keepdims=True, axis=(0, 1, 2))
+    variance = np.var(batch_of_images, keepdims=True, axis=(0, 1, 2))
+    return (batch_of_images - mean) / variance
+
+# listing 11.22 using the transformer encoder for text classification
+
+vocab_size = 20000
+embed_dim = 256
+num_heads = 2
+dense_dim = 32
+
+inputs = keras.Input(shape=(None,), dtype="int64")
+x = layers.Embedding(vocab_size, embed_dim)(inputs)
+x = TransformerEncoder(embed_dim, dense_dim, num_heads)(x)
+x = layers.GlobalMaxPooling1D()(x)
+x = layers.Dropout(0.5)(x)
+
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs, outputs)
+
+model.compile(
+    optimizer="rmsprop",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.summary()
+
+# listing 11.23 training and evaluating the transformer encoder based model
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("transformer_encoder.keras", save_best_only=True)
+]
+
+model.fit(int_train_ds, validation_data=int_val_ds, epochs=20, callbacks=callbacks)
+
+model = keras.models.load_model("transformer_encoder.keras", custom_objects={"TransformerEncoder": TransformerEncoder})
+print(f"Test acc: {model.evaluate(int_test_ds)[1]:.3f}")
+
+
+# listing 11.24 implementing positional embedding as a subclassed layer
+
+class PositionalEmbedding(layers.Layer):
+    def __init__(self, sequence_length, input_dim, output_dim, ** kwargs):
+        super().__init__(**kwargs)
+        self.token_embeddings = layers.Embedding(
+            input_dim = input_dim,
+            output_dim = output_dim
+        )
+        self.position_embeddings = layers.Embedding(
+            input_dim = sequence_length,
+            output_dim = output_dim
+        )
+        self.sequence_length = sequence_length
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+
+    def call(self, inputs):
+        length = tf.shape(inputs)[-1]
+        positions = tf.range(start=0, limit=length, delta=1)
+        embedded_tokens = self.token_embeddings(inputs)
+        embedded_positions = self.position_embeddings(positions)
+        return embedded_tokens + embedded_positions
+    
+    def compute_mask(self, inputs, mask=None):
+        return tf.math.not_equal(inputs, 0)
+    
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "output_dim": self.output_dim,
+            "sequence_length": self.sequence_length,
+            "input_dim": self.input_dim
+        })
+        return config
+    
+
+# listing 11.25 combining the transformer decoder with positional embedding
+
+vocab_size = 20000
+sequence_length = 600
+embed_dim = 256
+num_heads = 2
+dense_dim = 32
+
+inputs = keras.Input(shape=(None,), dtype="int64")
+x = PositionalEmbedding(sequence_length, vocab_size, embed_dim)(inputs)
+x = TransformerEncoder(embed_dim, dense_dim, num_heads)(x)
+x = layers.GlobalMaxPooling1D()(x)
+x = layers.Dropout(0.5)(x)
+outputs = keras.Model(inputs, outputs)
+model.compile(
+    optimizer="rmsprop",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.summary()
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("full_transformer_encoder.keras", save_best_only=True)
+]
+
+model.fit(
+    int_train_ds,
+    validation_data=int_val_ds,
+    epochs=20,
+    callbacks=callbacks
+)
+
+model = keras.models.load_model(
+    "full_transformer_encoder.keras",
+    custom_objects={"TransformerEncoder": TransformerEncoder,
+                    "PositionalEmbedding": PositionalEmbedding}
+)
+
+print(f"Test acc: {model.evaluate(int_test_ds)[1]:.3f}")
+
+# examples with the dataset
+# !wget http:/ /storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip
+
+
+# listing 11.26 vectorizing the english and spanish text pairs
+
