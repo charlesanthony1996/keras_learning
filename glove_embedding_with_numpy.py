674c05b549dda2cb45dfea6d992af72d98338ec5
charlesanthony1996 <charlesanthony296@gmail.com>
2025-04-06 21:18:44 +0200

    minor changes to the glove file

--- glove_embedding_with_numpy.py
+++ glove_embedding_with_numpy.py
@@ -16,3 +16,49 @@ def softmax(x):
     return e_x / np.sum(e_x)
 
 
+def attention(query, keys, values):
+    scores = np.dot(keys, query)
+    weights = softmax(scores)
+    return np.dot(weights, values)
+
+def feed_forward(x):
+    variation = np.array([(i % 5) - 2 for i in range(len(x))], dtype=np.float32)
+    h = x * variation
+    return np.maximum(0, h)
+
+def cosine_similarity(v1, v2):
+    norm1 = np.linalg.norm(v1)
+    norm2 = np.linalg.norm(v2)
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+    return np.dot(v1, v2) / (norm1 * norm2)
+
+def predict_next(output_vector, all_embeddings, avoid_word):
+    best_word = None
+    best_sim = -1
+    for word, emb in all_embeddings.items():
+        if word in ["<start>", avoid_word]:
+            continue
+        sim = cosine_similarity(output_vector, emb)
+        if sim > best_sim:
+            best_sim = sim
+            best_word = word
+    
+    return best_word
+
+
+# run the model
+encoder_word = "rapping"
+glove_path = "glove.6B.300d.txt"
+word_embeddings = load_glove_embeddings(glove_path)
+
+dim = len(next(iter(word_embeddings.values())))
+word_embeddings["<start>"] = np.zeros(dim, dtype=np.float32)
+
+if encoder_word not in word_embeddings:
+    print(f"{encoder_word} not found in glove embeddings")
+    exit(1)
+
+# should print out 300 because of the number of dimensions. read the name of the dataset again
+# print(dim)
+
